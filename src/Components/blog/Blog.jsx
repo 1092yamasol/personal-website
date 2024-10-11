f2c08f11bb0a04852b43d3ad547a7bdcb229883d
1092yamasol <a2n3t4e@gmail.com>
2024-10-11 13:36:52 +0300

    bloga and testmonial

--- src/Components/blog/Blog.jsx
+++ src/Components/blog/Blog.jsx
@@ -1,7 +1,72 @@
 import React from "react";
+import Image1 from "../../assets/luffy.jpg";
 import "./blog.css";
 function Blog() {
-  return <div>Blog</div>;
+  return (
+    <section className="blog container section" id="blog">
+      <h2 className="section__title">Latest Posts</h2>
+      <div className="blog__container grid">
+        <div className="blog__card">
+          <div className="blog__thumb">
+            <a href="3">
+              <span className="blog__category">Reviews</span>
+            </a>
+            <a href="#">
+              <img src={Image1} alt="" className="blog__img" />
+            </a>
+          </div>
+          <div className="blog__details">
+            <h3 className="blog__title">
+              5 Best App Development Tool for your projects
+            </h3>
+            <div className="blog__meta">
+              <span>09 Feburary,2024</span>
+              <span className="blog__dot">.</span>
+              <span>Anteneh</span>
+            </div>
+          </div>
+        </div>
+        <div className="blog__card">
+          <div className="blog__thumb">
+            <a href="3">
+              <span className="blog__category">Tutorial</span>
+            </a>
+            <a href="#">
+              <img src={Image1} alt="" className="blog__img" />
+            </a>
+          </div>
+          <div className="blog__details">
+            <h3 className="blog__title">Common Misconceptions About Payment</h3>
+            <div className="blog__meta">
+              <span>24 April,2024</span>
+              <span className="blog__dot">.</span>
+              <span>Anteneh</span>
+            </div>
+          </div>
+        </div>
+        <div className="blog__card">
+          <div className="blog__thumb">
+            <a href="3">
+              <span className="blog__category">Bussiness</span>
+            </a>
+            <a href="#">
+              <img src={Image1} alt="" className="blog__img" />
+            </a>
+          </div>
+          <div className="blog__details">
+            <h3 className="blog__title">
+              3 Things to know about startup bussiness
+            </h3>
+            <div className="blog__meta">
+              <span>1 October,2024</span>
+              <span className="blog__dot">.</span>
+              <span>Anteneh</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Blog;
--- src/Components/testimonials/Testimonials.jsx
+++ src/Components/testimonials/Testimonials.jsx
@@ -1,7 +1,67 @@
 import React from "react";
 import "./testimonials.css";
+import { title } from "framer-motion/client";
+import Image1 from "../../assets/luffy.jpg";
+import { Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import "swiper/css/pagination";
+
+const data = [
+  {
+    id: 1,
+    image: Image1,
+    title: "John Doe",
+    subtitle: "Product designer at Dribble",
+    comment:
+      "I Enjoy working with the theme and learn so much. you guys make the process fun and intersting. Good luck! 👍",
+  },
+  {
+    id: 2,
+    image: Image1,
+    title: "John Doe",
+    subtitle: "Product designer at Dribble",
+    comment:
+      "I Enjoy working with the theme and learn so much. you guys make the process fun and intersting. Good luck! 👍",
+  },
+  {
+    id: 3,
+    image: Image1,
+    title: "John Doe",
+    subtitle: "Product designer at Dribble",
+    comment:
+      "I Enjoy working with the theme and learn so much. you guys make the process fun and intersting. Good luck! 👍",
+  },
+];
 function Testimonials() {
-  return <div>Testimonials</div>;
+  return (
+    <section className="testimonials container section">
+      <h2 className="section__title">Clients & Reviews</h2>
+      <Swiper
+        className="testimonials__container grid"
+        modules={[Pagination]}
+        spaceBetween={30}
+        slidesPerView={1}
+        loop={true}
+        grabCursor={true}
+        pagination={{ clickable: true }}
+      >
+        {data.map(({ id, image, title, subtitle, comment }) => {
+          return (
+            <SwiperSlide className="testimonials__item" key={id}>
+              <div className="thumb">
+                <img src={image} alt="" />
+              </div>
+              <h3 className="testimonials__title">{title}</h3>
+              <span className="subtitle">{subtitle}</span>
+              <div className="comment">{comment}</div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
 }
 
 export default Testimonials;
